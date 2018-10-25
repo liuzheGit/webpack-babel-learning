278d4ddff1d86e5218642f687639b8f47d815030
liuzhe <zhethink@gmail.com>
2018-10-25 11:55:04 +0800

    分离出来

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['es2015']
-          }
+          loader: 'babel-loader'
         }
       }
     ]
